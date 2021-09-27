1cbcb3bf175a5e3bc06b672fc2184ee42cee1bc3
Tongtong Song <songtongmail@163.com>
2021-09-27 18:08:08 +0800

    update

--- experiments/02-speech-recognition/HMM/hmm.py
+++ experiments/02-speech-recognition/HMM/hmm.py
@@ -1,10 +1,25 @@
 # HMM Algorithm
-# Author: Tongtong Song (TJU)
+# Author: songtongmail@163.com (Tongtong Song (TJU))
 # Date: 2021/8/28 16:00
-# Last modified: 2021/9/5 15:50
-
+# Last modified: 2021/9/27 11:27
+import os
 import numpy as np
-from utils import init_logger
+import logging
+def init_logger(log_file=None):
+    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.handlers = [console_handler]
+
+    if log_file and log_file != '':
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+
+    return logger
 class HMM:
     def __init__(self, pi, A, B):
         super().__init__()
@@ -43,7 +58,6 @@ class HMM:
             \beta_{t}(i) = \sum_{j=1}^M(\beta_{t+1}(j)a_{ij}b_{j}(o_{t+1}))
         :param O:
         :return:
-
         """
         # definition backward probability matrix
         self.o_len = len(O)
@@ -53,9 +67,10 @@ class HMM:
         self.backward_prob_matrix[-1,:] = 1
 
         # t=T-2:-1, compute backward probability matrix
-       """
+        """
             TODO
-       """
+        """
+
         # get the first time total backward probability
         backward_prob = np.sum(self.pi*self.B[:,O[0]]*self.backward_prob_matrix[0])
 
@@ -86,6 +101,7 @@ class HMM:
             best_path:
             best_prob:
         """
+
         #  definition the best probability matrix and last node maxtrix
         o_len = len(O)
         best_prob_matrix = np.zeros((o_len,self.s_set_num))
@@ -94,11 +110,11 @@ class HMM:
         # t=0
         best_prob_matrix[0,:] = self.pi * self.B[:,O[0]]
         last_node_matrix[0,:] = -1
+
         # T=1:T, compute best probability matrix and last node matrix
         """
             TODO
         """
-
         # t=T-1, get best_prob and the last node
         last_node =  np.argmax(best_prob_matrix[o_len-1,:])
         best_prob = best_prob_matrix[o_len - 1,last_node]
@@ -109,31 +125,34 @@ class HMM:
             last_node = last_node_matrix[t, last_node]
             best_path.append(last_node)
 
-        return reversed(best_path), best_prob
+        # reverse to the noraml path
+        best_path = best_path[::-1]
+
+        return best_path, best_prob
 
 if __name__ == '__main__':
+
     result_path = './result'
     if not os.path.exists(result_path):
         os.mkdir(result_path)
     logger = init_logger(log_file='result/hmm.log')
     pi = np.array([0.2, 0.4, 0.4])
     A = np.array([[0.5, 0.2, 0.3],
-         [0.3, 0.5, 0.2],
-         [0.2, 0.3, 0.5]])
+                  [0.3, 0.5, 0.2],
+                  [0.2, 0.3, 0.5]])
     B = np.array([[0.5, 0.5],
-         [0.4, 0.6],
-         [0.7, 0.3]])
+                  [0.4, 0.6],
+                  [0.7, 0.3]])
 
     # observation sequence
-    O = [0,1,0]
+    O = [0, 1, 0]
 
-    hmm_model = HMM(pi,A,B)
+    hmm_model = HMM(pi, A, B)
     forward_prob = hmm_model.forward(O)
     logger.info('forward_prob:{:.6f}'.format(forward_prob))
     backward_prob = hmm_model.backward(O)
     logger.info('backward_prob:{:.6f}'.format(backward_prob))
-    prob = hmm_model.get_state_probability(2,2)
+    prob = hmm_model.get_state_probability(2, 2)
     logger.info('state(2,2) prob:{:.6f}'.format(prob))
     best_path, best_prob = hmm_model.decoding(O)
-    logger.info('best_path:{},best_prob:{:.6f}'.format(best_path,best_prob))
-
+    logger.info('best_path:{},best_prob:{:.6f}'.format(best_path, best_prob))
